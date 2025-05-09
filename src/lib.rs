3cbc100ea74d3c839c9e8379185c8dbd8143ed64
Smuu <18609909+Smuu@users.noreply.github.com>
2025-05-09 12:05:08 +0200

    feat: more tests
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,10 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn it_works_2() {
+        let result = add(3, 3);
+        assert_eq!(result, 6);
+    }
 }
